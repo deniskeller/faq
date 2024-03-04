3e71a8af4dab26a3d32b7d68903195f5bd175718
deniskeller <dentean@mail.ru>
2024-03-04 20:07:02 +0400

    git init

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const path = require("path");
+const withImages = require("next-images");
+
+module.exports = withImages({
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/assets/scss")],
+    prependData: `@import "main.scss";`,
+  }
+});
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+// import { Landing } from '@layouts/index';
+// import { Main } from '@view/landing';
+
+const MainPage = () => {
+  return (
+    // <Landing>
+    //   <Main />
+    // </Landing>
+    <h1>Main page</h1>
+  );
+};
+
+export default MainPage;
