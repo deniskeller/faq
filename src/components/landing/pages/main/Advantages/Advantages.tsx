2ef1eeaa6ace028dc7544a447f65f7e17ec4b671
deniskeller <dentean@mail.ru>
2024-03-08 19:01:30 +0400

    Advantages block showmore animation

--- src/components/landing/pages/main/Advantages/Advantages.tsx
+++ src/components/landing/pages/main/Advantages/Advantages.tsx
@@ -30,7 +30,7 @@ const Advantages = () => {
 
         <div
           className={s.Advantages_Content}
-          style={{ maxHeight: showMore ? '1999px' : '734px' }}
+          style={{ maxHeight: showMore && isMobile ? '1999px' : '734px' }}
         >
           {/* 1 */}
           <Link
